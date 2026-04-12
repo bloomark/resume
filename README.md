1f40f87b24fc59194cac100dc3aa034c857d3d50
Adi Venkatesh <adithy@amazon.com>
2026-04-11 18:11:14 -0700

    feat: Add Nix flake for reproducible resume builds
    Provides `nix build` for hermetic PDF compilation and `nix develop`
    for an interactive shell with xelatex. No manual TeX installation
    needed on any machine.

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-### Prerequisites
-[TinyTeX](https://github.com/rstudio/tinytex) (installed at `~/Library/TinyTeX`)
+### Build (Nix)
+```
+nix build
+```
+Output: `result/cv_3.pdf`
 
-### Usage
-`xelatex cv_3.tex`
+### Dev shell
+```
+nix develop
+xelatex cv_3.tex
+```
+
+### Troubleshooting
+
+**`experimental Nix feature 'nix-command' is disabled`**
+
+Add to `~/.config/nix/nix.conf`:
+```
+experimental-features = nix-command flakes
+```
+
+**View build logs**
+```
+nix log .
+```
---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "Adithya Venkatesh's resume";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tex = pkgs.texliveSmall.withPackages (ps: [
+            ps.xetex
+            ps.fontspec
+            ps.xltxtra
+            ps.xunicode
+            ps.realscripts
+            ps.metalogo
+            ps.marvosym
+            ps.xcolor
+            ps.geometry
+            ps.fancyhdr
+            ps.hyperref
+            ps.graphics
+            ps.tools
+            ps.tipa
+            ps.greek-fontenc
+          ]);
+        in
+        {
+          default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "cv";
+            version = "0.1.0";
+            src = ./.;
+            buildInputs = [ tex ];
+            buildPhase = ''
+              xelatex cv_3.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp cv_3.pdf $out/
+            '';
+          };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tex = pkgs.texliveSmall.withPackages (ps: [
+            ps.xetex
+            ps.fontspec
+            ps.xltxtra
+            ps.xunicode
+            ps.realscripts
+            ps.metalogo
+            ps.marvosym
+            ps.xcolor
+            ps.geometry
+            ps.fancyhdr
+            ps.hyperref
+            ps.graphics
+            ps.tools
+            ps.tipa
+            ps.greek-fontenc
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ tex ];
+          };
+        }
+      );
+    };
+}
